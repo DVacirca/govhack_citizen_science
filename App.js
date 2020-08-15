207937f1e2645eee87f835ea02962d833da9423a
Dante Vacirca <dante.vacirca1@gmail.com>
2020-08-15 21:52:05 +1000

    added packages for routing

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
+import { MyStack } from './src/pages/Stack';
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MyStack />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +16,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#F8F0DE',
     alignItems: 'center',
     justifyContent: 'center',
   },
